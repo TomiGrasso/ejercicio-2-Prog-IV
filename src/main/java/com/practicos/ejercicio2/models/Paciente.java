c348edf905bb12a02ccaab8f49020a6551dc49d0
TomiGrasso <tomiigrasso@gmail.com>
2026-03-29 18:06:34 -0300

    Creación clase 'Paciente'

--- src/main/java/com/practicos/ejercicio2/models/Paciente.java
+++ src/main/java/com/practicos/ejercicio2/models/Paciente.java
@@ -1,4 +1,63 @@
 package com.practicos.ejercicio2.models;
 
+import java.util.Date;
+
 public class Paciente {
+
+    private Long id;
+    private Integer dni;
+    private String nombre;
+    private String apellido;
+    private Date fechaNacimiento;
+
+    public Paciente() {
+    }
+
+    public Paciente(Long id, Integer dni, String nombre, String apellido, Date fechaNacimiento) {
+        this.id = id;
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getDni() {
+        return dni;
+    }
+
+    public void setDni(Integer dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
 }
