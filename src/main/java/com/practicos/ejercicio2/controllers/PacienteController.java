4644d614cdf828f4e5f28fe7d588495b2124ff15
TomiGrasso <tomiigrasso@gmail.com>
2026-03-30 21:26:50 -0300

    Agregué validaciones

--- src/main/java/com/practicos/ejercicio2/controllers/PacienteController.java
+++ src/main/java/com/practicos/ejercicio2/controllers/PacienteController.java
@@ -1,7 +1,9 @@
 package com.practicos.ejercicio2.controllers;
 
 import com.practicos.ejercicio2.models.Paciente;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,5 +34,27 @@ public class PacienteController {
     @GetMapping("/menores")
     public List<Paciente> obtenerPacientesMenores(){
         return pacientesMenores;
+    }
+
+    private void validarPaciente(Paciente paciente){
+        if (paciente == null){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Debe enviar los datos del paciente.");
+        }
+
+        if (paciente.getDni() == null || paciente.getDni() < 0){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Debe ingresar el DNI y tiene que ser mayor a 0.");
+        }
+
+        if (paciente.getNombre() == null || paciente.getNombre().isBlank()){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Debe ingresar el nombre del paciente,");
+        }
+
+        if (paciente.getApellido() == null || paciente.getApellido().isBlank()){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Debe ingresar el apellido del paciente,");
+        }
+
+        if (paciente.getFechaNacimiento() == null){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Debe ingresar la fecha de nacimiento del paciente,");
+        }
     }
 }
